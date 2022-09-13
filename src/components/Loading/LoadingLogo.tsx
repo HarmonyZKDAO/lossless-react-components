316630933efd758d3135b269b454b3706e7f473c
vplasencia <v.pcalana@gmail.com>
2022-09-13 00:07:16 -0400

    Change Loading Logo

--- src/components/Loading/LoadingLogo.tsx
+++ src/components/Loading/LoadingLogo.tsx
@@ -1,22 +1,20 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import classnames from 'classnames'
 
-import { ThemeContext } from '../ThemeContextProvider'
+// import { ThemeContext } from '../ThemeContextProvider'
 import { LoadingDots } from './LoadingDots'
 
-import LogoDark from '../../assets/PoolTogetherLogos/pooltogether-p.svg'
-import Logo from '../../assets/PoolTogetherLogos/pooltogether-p-purple.svg'
+// import LogoDark from '../../assets/PoolTogetherLogos/pooltogether-p.svg'
+// import Logo from '../../assets/PoolTogetherLogos/pooltogether-p-purple.svg'
 
-export function LoadingLogo (props) {
-  const { theme } = useContext(ThemeContext)
+import Logo from '../../assets/LosslessLogo/Lossless_favicon.png'
+
+export function LoadingLogo(props) {
+  // const { theme } = useContext(ThemeContext)
 
   return (
     <div className={classnames('flex flex-col', props.className)} style={{ width: 'min-content' }}>
-      <img
-        src={theme === 'dark' ? LogoDark : Logo}
-        className='w-8 mx-auto'
-        style={{ borderWidth: 0 }}
-      />
+      <img src={Logo} className='w-16 mx-auto' style={{ borderWidth: 0 }} />
       <LoadingDots />
     </div>
   )
