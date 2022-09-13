e644974ff6fdec01469f9161f5f5557770bc6fe3
vplasencia <v.pcalana@gmail.com>
2022-09-13 10:08:47 -0400

    Change header Logo

--- src/components/PageHeader/HeaderLogo.tsx
+++ src/components/PageHeader/HeaderLogo.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import classNames from 'classnames'
 
-import MobileLogoDark from '../../assets/PoolTogetherLogos/pooltogether-p-purple.svg'
-import MobileLogo from '../../assets/PoolTogetherLogos/pooltogether-p.svg'
-import DesktopLogoDark from '../../assets/PoolTogetherLogos/pooltogether-full-logo-purple.svg'
-import DesktopLogo from '../../assets/PoolTogetherLogos/pooltogether-full-logo.svg'
+// import MobileLogoDark from '../../assets/PoolTogetherLogos/pooltogether-p-purple.svg'
+// import MobileLogo from '../../assets/PoolTogetherLogos/pooltogether-p.svg'
+// import DesktopLogoDark from '../../assets/PoolTogetherLogos/pooltogether-full-logo-purple.svg'
+// import DesktopLogo from '../../assets/PoolTogetherLogos/pooltogether-full-logo.svg'
+
+import MobileLogo from '../../assets/LosslessLogo/Lossless_favicon.png'
+import DesktopLogo from '../../assets/LosslessLogo/Lossless_logo.svg'
 
 export const HeaderLogo = (props) => {
   const { desktopSizeClassNames, centered } = props
@@ -19,21 +22,10 @@ export const HeaderLogo = (props) => {
         className={classNames('hidden sm:dark:block', desktopSizeClassNames, extendedClassNames)}
         src={DesktopLogo}
       />
-      <ImageContainer
-        className={classNames(
-          'hidden dark:hidden sm:block',
-          desktopSizeClassNames,
-          extendedClassNames
-        )}
-        src={DesktopLogoDark}
-      />
+
       <ImageContainer
         className={classNames('hidden dark:block sm:dark:hidden w-7 h-12', extendedClassNames)}
         src={MobileLogo}
-      />
-      <ImageContainer
-        className={classNames('block sm:hidden dark:hidden w-7 h-12', extendedClassNames)}
-        src={MobileLogoDark}
       />
     </>
   )
@@ -43,7 +35,7 @@ const ImageContainer = (props) => {
   const { className, ...imageProps } = props
   return (
     <div className={classNames(className)}>
-      <img {...imageProps} alt='PoolTogether Logo' />
+      <img {...imageProps} alt='Lossless Logo' />
     </div>
   )
 }
